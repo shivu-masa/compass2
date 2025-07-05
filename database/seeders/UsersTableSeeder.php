93c2cc33cba300a4ef2aa9df96aeb5d55966459b
DESKTOP-DV602N4\15ht2 <shivuya.masaki0814@gmail.com>
2025-07-05 10:23:22 +0900

    no message

--- database/seeders/UsersTableSeeder.php
+++ database/seeders/UsersTableSeeder.php
@@ -3,6 +3,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class UsersTableSeeder extends Seeder
 {
@@ -13,6 +16,18 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-
+        DB::table('users')->insert([
+            'over_name' => '山田',
+            'under_name' => '太郎',
+            'over_name_kana' => 'ヤマダ',
+            'under_name_kana' => 'タロウ',
+            'mail_address' => 'yamada@example.com',
+            'sex' => 1,
+            'birth_day' => '1990-01-01',
+            'role' => 1,
+            'password' => Hash::make('password123'),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
